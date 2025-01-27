bbac2e7f1386ec10238810272496710b5f1cad22
chokopay <vazovmikola@gmail.com>
2025-01-27 13:47:57 +0100

    beta v0.2
    add game object finish game screen

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -27,4 +27,20 @@ public class GameScreen {
             System.out.println();
         }
     }
+    public void ClearScreen(int x,int y){
+        this.screenBack[y][x] ='.';
+    }
+    public int getWidth(){
+        return this.width;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+    public char getObjectOnLocation(int x, int y){
+        return this.screenBack[y][x];
+    }
+
+    public void setOobjectOnLocation(GameObject object, int x,int y){
+        this.screenBack[y][x]= object.getSymbol();
+    }     
 }
