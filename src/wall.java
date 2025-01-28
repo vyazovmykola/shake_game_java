af7fd697373b5cb20857e92ac12f80b72f670c45
chokopay <vazovmikola@gmail.com>
2025-01-28 21:53:32 +0100

    beta v0.3
    add wall.java (not working)

---
+++ src/wall.java
@@ -0,0 +1,22 @@
+package src;
+/* 
+public class wall extends GameObject {
+    public wall (){
+        setSymbol("#");
+    }
+    public wall(char symbol){
+        setSymbol(symbol);
+    }
+    public void addWallsRow(GameScreen screen, Wall wall, int rowNumber) {
+		for (int i = 0; i < screen.getScreenWidth(); i++) {
+			screen.setObjectOnLocation(wall, i, rowNumber);
+		}
+	}
+
+	public void addWallsColumn(GameScreen screen, Wall wall, int columnNumber) {
+		for (int i = 0; i < screen.getScreenHeight(); i++) {
+			screen.setObjectOnLocation(wall, columnNumber, i);
+		}
+	}
+}
+*/
